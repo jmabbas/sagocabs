6a30216923c92eeb4da0cb9e217befcbfa96ecb8
jmabbas <mdabbas.it@gmail.com>
2025-10-05 18:01:44 +0530

    stats added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/Header'
+import Stats from './components/Stats'
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className='font-poppins'>
         <Header/>
+        <Stats />
       </div>
     </>
   )
